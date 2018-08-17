957f5485cdaf29a58c861fa5d278166bbe30b088
Pedro Henrique Ficuciello Mendes <phficuciello@gmail.com>
2018-08-17 02:19:23 -0300

    Add files via upload

---
+++ javascript/js/funcoes.js
@@ -0,0 +1,6 @@
+function trocaImagem () {
+	document.get.ElementById("myImage").arc="img/html-logo-badge-512.png";
+}
+function imagemTroca () {
+	document.get.ElementById("myImage").arc="img/js-logo-badge-512.png";
+}
