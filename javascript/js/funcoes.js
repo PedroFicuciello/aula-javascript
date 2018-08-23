731b62b4b4d6a3b1a8951ca6b529120c444fdccf
PedroFicuciello <phficuciello@gmail.com>
2018-08-23 20:25:49 -0300

    Alteração de Fonte

--- javascript/js/funcoes.js
+++ javascript/js/funcoes.js
@@ -1,6 +1,9 @@
 function trocaImagem () {
-	document.get.ElementById("myImage").arc="img/html-logo-badge-512.png";
+	document.getElementById("myImage").src="img/html-logo-badge-512.png";
 }
 function imagemTroca () {
-	document.get.ElementById("myImage").arc="img/js-logo-badge-512.png";
+	document.getElementById("myImage").src="img/js-logo-badge-512.png";
+}
+function trocaFonte () {
+	document.getElementById("demo").style.fontSize="35px";
 }
